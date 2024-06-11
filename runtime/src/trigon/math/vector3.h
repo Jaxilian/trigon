40c192cae14591603656433d5bf0b6e041388666
Jaxilian.C <jax.carls@gmail.com>
2024-06-11 11:41:24 +0300

    update

--- runtime/src/trigon/math/vector3.h
+++ runtime/src/trigon/math/vector3.h
@@ -4,20 +4,21 @@
 #include <cstring> 
 #include <cmath>
 
-class vector2_t {
+class vector3_t {
 public:
-    vector2_t(float x_val, float y_val)
-        : x(_data[0]), y(_data[1]) {
+    vector3_t(float x_val, float y_val, float z_val)
+        : x(_data[0]), y(_data[1]), z(_data[2]) {
         _data[0] = x_val;
         _data[1] = y_val;
-        _data[2] = 0.0f;
-        _data[3] = 0.0f;
+        _data[2] = z_val;
+        _data[3] = 0.0f; 
     }
 
     const float& x;
     const float& y;
+    const float& z;
 
-    inline vector2_t& operator+=(const vector2_t& other) {
+    inline vector3_t& operator+=(const vector3_t& other) {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 result = _mm_add_ps(this_vec, other_vec);
@@ -25,7 +26,7 @@ public:
         return *this;
     }
 
-    inline vector2_t& operator-=(const vector2_t& other) {
+    inline vector3_t& operator-=(const vector3_t& other) {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 result = _mm_sub_ps(this_vec, other_vec);
@@ -33,7 +34,7 @@ public:
         return *this;
     }
 
-    inline vector2_t& operator*=(const vector2_t& other) {
+    inline vector3_t& operator*=(const vector3_t& other) {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 result = _mm_mul_ps(this_vec, other_vec);
@@ -41,7 +42,7 @@ public:
         return *this;
     }
 
-    inline vector2_t& operator/=(const vector2_t& other) {
+    inline vector3_t& operator/=(const vector3_t& other) {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 result = _mm_div_ps(this_vec, other_vec);
@@ -49,15 +50,15 @@ public:
         return *this;
     }
 
-    inline vector2_t& operator=(const vector2_t& other) {
+    inline vector3_t& operator=(const vector3_t& other) {
         if (this != &other) {
             std::memcpy(_data, other._data, sizeof(_data));
         }
         return *this;
     }
 
-    inline vector2_t operator+(const vector2_t& other) const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator+(const vector3_t& other) const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 sum = _mm_add_ps(this_vec, other_vec);
@@ -65,8 +66,8 @@ public:
         return result;
     }
 
-    inline vector2_t operator-(const vector2_t& other) const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator-(const vector3_t& other) const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 diff = _mm_sub_ps(this_vec, other_vec);
@@ -74,8 +75,8 @@ public:
         return result;
     }
 
-    inline vector2_t operator*(const vector2_t& other) const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator*(const vector3_t& other) const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 prod = _mm_mul_ps(this_vec, other_vec);
@@ -83,8 +84,8 @@ public:
         return result;
     }
 
-    inline vector2_t operator/(const vector2_t& other) const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator/(const vector3_t& other) const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 quot = _mm_div_ps(this_vec, other_vec);
@@ -92,8 +93,8 @@ public:
         return result;
     }
 
-    inline vector2_t operator*(float scalar) const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator*(float scalar) const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 scalar_vec = _mm_set1_ps(scalar);
         __m128 prod = _mm_mul_ps(this_vec, scalar_vec);
@@ -101,7 +102,7 @@ public:
         return result;
     }
 
-    inline vector2_t& operator*=(float scalar) {
+    inline vector3_t& operator*=(float scalar) {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 scalar_vec = _mm_set1_ps(scalar);
         __m128 result = _mm_mul_ps(this_vec, scalar_vec);
@@ -109,7 +110,7 @@ public:
         return *this;
     }
 
-    inline vector2_t& operator+=(float num) {
+    inline vector3_t& operator+=(float num) {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 num_vec = _mm_set1_ps(num);
         __m128 result = _mm_add_ps(this_vec, num_vec);
@@ -117,8 +118,8 @@ public:
         return *this;
     }
 
-    inline vector2_t operator+(float num) const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator+(float num) const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 num_vec = _mm_set1_ps(num);
         __m128 sum = _mm_add_ps(this_vec, num_vec);
@@ -126,7 +127,7 @@ public:
         return result;
     }
 
-    inline vector2_t& operator-=(float num) {
+    inline vector3_t& operator-=(float num) {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 num_vec = _mm_set1_ps(num);
         __m128 result = _mm_sub_ps(this_vec, num_vec);
@@ -134,8 +135,8 @@ public:
         return *this;
     }
 
-    inline vector2_t operator-(float num) const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator-(float num) const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 num_vec = _mm_set1_ps(num);
         __m128 sum = _mm_sub_ps(this_vec, num_vec);
@@ -143,16 +144,16 @@ public:
         return result;
     }
 
-    inline bool operator==(const vector2_t& other) const {
-        return _data[0] == other._data[0] && _data[1] == other._data[1];
+    inline bool operator==(const vector3_t& other) const {
+        return _data[0] == other._data[0] && _data[1] == other._data[1] && _data[2] == other._data[2];
     }
 
-    inline bool operator!=(const vector2_t& other) const {
-        return _data[0] != other._data[0] || _data[1] != other._data[1];
+    inline bool operator!=(const vector3_t& other) const {
+        return _data[0] != other._data[0] || _data[1] != other._data[1] || _data[2] != other._data[2];
     }
 
-    inline vector2_t operator-() const {
-        vector2_t result(0.0f, 0.0f);
+    inline vector3_t operator-() const {
+        vector3_t result(0.0f, 0.0f, 0.0f);
         __m128 this_vec = _mm_load_ps(_data);
         __m128 neg_vec = _mm_sub_ps(_mm_setzero_ps(), this_vec);
         _mm_store_ps(result._data, neg_vec);
@@ -167,17 +168,17 @@ public:
         return _mm_cvtss_f32(_mm_sqrt_ss(sum));
     }
 
-    inline vector2_t normalized() const {
+    inline vector3_t normalized() const {
         float mag = magnitude();
         __m128 this_vec = _mm_load_ps(_data);
         __m128 mag_vec = _mm_set1_ps(mag);
         __m128 norm_vec = _mm_div_ps(this_vec, mag_vec);
-        vector2_t result(0.0f, 0.0f);
+        vector3_t result(0.0f, 0.0f, 0.0f);
         _mm_store_ps(result._data, norm_vec);
         return result;
     }
 
-    inline float dot(const vector2_t& other) const {
+    inline float dot(const vector3_t& other) const {
         __m128 this_vec = _mm_load_ps(_data);
         __m128 other_vec = _mm_load_ps(other._data);
         __m128 prod = _mm_mul_ps(this_vec, other_vec);
@@ -186,29 +187,33 @@ public:
         return _mm_cvtss_f32(sum);
     }
 
-    inline float cross(const vector2_t& other) const {
-        return _data[0] * other._data[1] - _data[1] * other._data[0];
+    inline vector3_t cross(const vector3_t& other) const {
+        return vector3_t(
+            _data[1] * other._data[2] - _data[2] * other._data[1],
+            _data[2] * other._data[0] - _data[0] * other._data[2],
+            _data[0] * other._data[1] - _data[1] * other._data[0]
+        );
     }
 
-    inline float angle_between(const vector2_t& other) const {
+    inline float angle_between(const vector3_t& other) const {
         float dot_prod = dot(other);
         float mags = magnitude() * other.magnitude();
         return std::acos(dot_prod / mags);
     }
 
-    inline vector2_t projection_onto(const vector2_t& other) const {
+    inline vector3_t projection_onto(const vector3_t& other) const {
         float dot_prod = dot(other);
         float other_mag_sq = other.magnitude() * other.magnitude();
         float scalar = dot_prod / other_mag_sq;
         return other * scalar;
     }
 
-    inline float distance(const vector2_t& other) const {
+    inline float distance(const vector3_t& other) const {
         return (*this - other).magnitude();
     }
 
-    //lerps from A to B where T is the normalize progression (0.0f-1.0f)
-    static inline vector2_t lerp(const vector2_t& a, const vector2_t& b, float t) {
+    // lerps from A to B where T is the normalized progression (0.0f-1.0f)
+    static inline vector3_t lerp(const vector3_t& a, const vector3_t& b, float t) {
         __m128 t_vec = _mm_set1_ps(t);
         __m128 a_vec = _mm_load_ps(a._data);
         __m128 b_vec = _mm_load_ps(b._data);
@@ -216,7 +221,7 @@ public:
         __m128 t_diff = _mm_mul_ps(diff, t_vec);
         __m128 result = _mm_add_ps(a_vec, t_diff);
 
-        vector2_t res(0.0f, 0.0f);
+        vector3_t res(0.0f, 0.0f, 0.0f);
         _mm_store_ps(res._data, result);
         return res;
     }
@@ -224,3 +229,4 @@ public:
 private:
     alignas(16) float _data[4];
 };
+
