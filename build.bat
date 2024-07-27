c0d2d88cf433046585fd2f5d2b09a34f3fa34114
Jaxilian <jax.carls@gmail.com>
2024-07-27 19:41:12 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
 @echo off
 setlocal enabledelayedexpansion
 
+if not exist "C:\Applications\Premake5" (
+    git clone https://github.com/Jaxilian/Premake5.git
+    xcopy Premake5 C:\Applications\Premake5 /s /e /I /Y
+      REM Delete the Premake5 folder after copying its contents
+    rd /s /q Premake5
+)
+
 
 premake5 vs2022
 msbuild trigon.sln -p:Configuration=release
