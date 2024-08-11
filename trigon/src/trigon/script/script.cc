a490123db8acee3d97134e8adf1450302249da3e
Jaxilian <jax.carls@gmail.com>
2024-08-11 15:02:34 +0200

    luau

--- trigon/src/trigon/script/script.cc
+++ trigon/src/trigon/script/script.cc
@@ -1,12 +1,130 @@
 #include "script.h"
 
-#include "lua.h"
-#include "lualib.h"
-#include "lauxlib.h"
+#include <lua.h>
+#include <lualib.h>
+#include <Luau/CodeGen.h>
+#include <Luau/Compiler.h>
+#include <Luau/Parser.h>
+#include <Luau/TimeTrace.h>
 
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+lua_State* L = luaL_newstate();
+
+struct Coverage{
+    lua_State* L = nullptr;
+    std::vector<int> functions;
+} gCoverage;
+
+struct GlobalOptions
+{
+    int optimizationLevel = 1;
+    int debugLevel = 1;
+} globalOptions;
+
+
+static Luau::CompileOptions copts()
+{
+    Luau::CompileOptions result = {};
+    result.optimizationLevel = globalOptions.optimizationLevel;
+    result.debugLevel = globalOptions.debugLevel;
+    result.typeInfoLevel = 1;
+    result.coverageLevel = (gCoverage.L != nullptr) ? 2 : 0;
+
+    return result;
+}
+
+static int lua_loadstring(lua_State* L)
+{
+    size_t l = 0;
+    const char* s = luaL_checklstring(L, 1, &l);
+    const char* chunkname = luaL_optstring(L, 2, s);
+
+    lua_setsafeenv(L, LUA_ENVIRONINDEX, false);
+
+    std::string bytecode = Luau::compile(std::string(s, l), copts());
+    if (luau_load(L, chunkname, bytecode.data(), bytecode.size(), 0) == 0)
+        return 1;
+
+    lua_pushnil(L);
+    lua_insert(L, -2);
+    return 2;
+}
+
+static int finishrequire(lua_State* L)
+{
+    if (lua_isstring(L, -1))
+        lua_error(L);
+
+    return 1;
+}
+
+static int lua_require(lua_State* L)
+{
+    std::string name = luaL_checkstring(L, 1);
+
+    RequireResolver::ResolvedRequire resolvedRequire = RequireResolver::resolveRequire(L, std::move(name));
+
+    if (resolvedRequire.status == RequireResolver::ModuleStatus::Cached)
+        return finishrequire(L);
+    else if (resolvedRequire.status == RequireResolver::ModuleStatus::NotFound)
+        luaL_errorL(L, "error requiring module");
+
+    // module needs to run in a new thread, isolated from the rest
+    // note: we create ML on main thread so that it doesn't inherit environment of L
+    lua_State* GL = lua_mainthread(L);
+    lua_State* ML = lua_newthread(GL);
+    lua_xmove(GL, L, 1);
+
+    // new thread needs to have the globals sandboxed
+    luaL_sandboxthread(ML);
+
+    // now we can compile & run module on the new thread
+    std::string bytecode = Luau::compile(resolvedRequire.sourceCode, copts());
+    if (luau_load(ML, resolvedRequire.chunkName.c_str(), bytecode.data(), bytecode.size(), 0) == 0)
+    {
+        if (codegen)
+        {
+            Luau::CodeGen::CompilationOptions nativeOptions;
+            Luau::CodeGen::compile(ML, -1, nativeOptions);
+        }
+
+        if (coverageActive())
+            coverageTrack(ML, -1);
+
+        int status = lua_resume(ML, L, 0);
+
+        if (status == 0)
+        {
+            if (lua_gettop(ML) == 0)
+                lua_pushstring(ML, "module must return a value");
+            else if (!lua_istable(ML, -1) && !lua_isfunction(ML, -1))
+                lua_pushstring(ML, "module must return a table or function");
+        }
+        else if (status == LUA_YIELD)
+        {
+            lua_pushstring(ML, "module can not yield");
+        }
+        else if (!lua_isstring(ML, -1))
+        {
+            lua_pushstring(ML, "unknown error while running module");
+        }
+    }
+
+    // there's now a return value on top of ML; L stack: _MODULES ML
+    lua_xmove(ML, L, 1);
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -4, resolvedRequire.absolutePath.c_str());
+
+    // L stack: _MODULES ML result
+    return finishrequire(L);
+}
 
 script_t::script_t() {
-
+	
 
 }
 
